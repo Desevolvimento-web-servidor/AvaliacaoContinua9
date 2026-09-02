e0cfe1ef03cdd1c66c2944d4cd3bd8a4a6f43516
RVinicius <ruza.vinicius@aluno.ifsp.edu.br>
2026-09-02 00:49:25 +0000

    Avaliacao Continua 6

--- hello.py
+++ hello.py
@@ -64,11 +64,15 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    usuarios = User.query.all()
     form = NameForm()
+
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
+
         if user is None:
-            user = User(username=form.name.data)
+            user_role = Role.query.filter_by(name='User').first()
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -76,5 +80,6 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
+
     return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+                           known=session.get('known', False), usuarios=usuarios)
